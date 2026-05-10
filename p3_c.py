a2f0c2d7742576599c4b6445cb4eaf8444c0fde5
timca <timca@minerva.cse.chalmers.se>
2026-05-10 14:14:45 +0200

    some stuff

--- p3_c.py
+++ p3_c.py
@@ -40,34 +40,6 @@ def main() -> None:
     # First get the exact number of distincs words 
     distinct_words_count = get_distinct_words("data/small")
 
-    results = []
-    for i in range(1000):
-        seed = f"0x{secrets.token_hex(4)}" # random 32-bit hex value
-        cmd = ["./assignment5_problem3.py", "-s", seed, "-m", str(registers), "-w", str(workers), f"data/small"]
-        process = subprocess.run(cmd, capture_output=True, text=True)
-        output = process.stdout
-
-        if process.returncode != 0:
-            print(f"Error: Subprocess failed with code in iteration {i}: {process.returncode}")
-            print(process.stderr)
-            print(process.stdout)
-            exit()
-        
-        match = re.search(r"Cardinality estimate: ([\d.]+)", output)
-        if not match:
-            print("Failed to find pattern in stdout")
-            exit()
-
-        results.append({
-            "seed": seed,
-            "estimate": match.group(1),
-        })
-         
-    with open("results_p3_c.csv", "w") as f:
-        writer = csv.DictWriter(f, fieldnames=["seed", "estimate"])
-        writer.writeheader()
-        writer.writerows(results)
-
     print(f"Correct number of distinct words: {distinct_words_count}")
 
 if __name__ == "__main__":
